af16d7856d791140a249dd2ba9eccf4b438d8ec0
常会军 <chjwrr@163.com>
2017-05-09 10:55:09 +0800

    iOS 原生扫描二维码，生成二维码
    iOS 原生扫描二维码，生成二维码

---
+++ iOSScan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSScan
+//
+//  Created by Mac on 2017/5/9.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
